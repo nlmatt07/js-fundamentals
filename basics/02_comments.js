fdf3f594b8e7bbba5a04dcd5a572a57f3b2fb5c3
Mateus Nadal <nlmatt07@hotmail.com>
2020-09-18 18:15:28 -0300

    comments

---
+++ basics/02_comments.js
@@ -0,0 +1,11 @@
+console.log('What is a commentary ?')
+
+// Wow, this is a commentary!
+
+
+/*
+This a comment also!!!
+And you can use multiple lines.
+*/
+
+// Now you can use many comments you want, but in time, You'll see why you shouldn't do it!
