91e403cf6ba3dc364031b0d778fe3bbe9135b45b
Mateus Nadal <nlmatt07@hotmail.com>
2020-09-18 17:54:27 -0300

    console.log

---
+++ basics/01_console_log.js
@@ -0,0 +1,20 @@
+console.log('Hello World');
+console.log('Hello World')
+
+console.log("Hello World");
+console.log('Hello World')
+
+console.log(`Hello World`);
+console.log(`Hello World`)
+
+console.log("Hello 'World'");
+console.log('Hello "World"');
+console.log(`"Hello" 'World'`);
+
+
+console.log(12345);
+console.log(12345, 15.50);
+console.log(12345, 15.50, "Hello World");
+
+
+console.log("Hi 👋🏻, my name is 'Matt', and i'm learning 'Javascript'")
